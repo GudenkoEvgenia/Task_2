136ab9751d81feb2ccfe669b589f0e2f05474d7f
Evgenia23 <Jekk58844109@gmail.com>
2023-06-24 21:07:39 +0300

    Task_5 error1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,24 +34,22 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: "", goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods = {title: goods[i].title, count: goods[i].count}; 
-        data.goods.push(gata.goods);
-    }
-
-    data.order.address = address;
-    address = {street: "", house: "", entrance: "", floor: "", flat: ""};
-    data.order.sum = sum;
-
-    data.client = name + phone;
-
-    let jsonData = JSON.stringify({data: data});
-
-    return jsonData;
+let data = {client: "", goods: [], order: {}};
+    
+let countOfGoods = goods.length;
+    
+for (let i = 0; i < countOfGoods; i += 1) {
+    data.goods.push({title: goods[i].title, count: goods[i].count});
+}
+    
+data.order.address = `${address.street}, ${address.house}, ${address.entrance}, ${address.floor}, ${address.flat}`;
+data.order.sum = sum;
+    
+data.client = `${name} ${phone}`;
+    
+let jsonData = JSON.stringify({data});
+    
+return jsonData;
 }
    
     
